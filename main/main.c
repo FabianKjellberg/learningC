bad8e3019eab5f8ff9fcbd7b8761436e38a8f6c6
FabianKjellberg <Fabian.Kjellberg98@gmail.com>
2026-04-19 22:31:22 +0200

    statefull led blinking

--- main/main.c
+++ main/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include "countdown.h"
+#include "led.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
-  countdown_task();
+  printf("Hello world\n");
+  
+  xTaskCreate(led_task, "led_task", 4096, NULL, 5, NULL);
+
+  while(1) {
+    printf("changing state to blue\n");
+    led_set_state(LED_STATE_BLINKING_BLUE);
+    vTaskDelay(pdMS_TO_TICKS(10000));
+
+    printf("changing state to red\n");
+    led_set_state(LED_STATE_BLINKING_RED);
+    vTaskDelay(pdMS_TO_TICKS(10000));
+
+    printf("changing state to green\n");
+    led_set_state(LED_STATE_BLINKING_GREEN);
+    vTaskDelay(pdMS_TO_TICKS(10000));
+  }
 }
 
